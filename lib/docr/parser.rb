ac0d002a899b09bf03a09dc0a7c4b4e5bce46b33
Jeremy McAnally <jeremymcanally@122.168.249.10.in-addr.arpa>
2008-04-06 09:33:28 -0400

    Start the refactoring!  Using ruby_parser now...

--- lib/docr/parser.rb
+++ lib/docr/parser.rb
@@ -1,234 +1,85 @@
-require 'rdoc/rdoc'
+require 'ruby_parser'
+require 'pp'
 
-['processed_token', 'stats', 'core_ext/string', 'rdoc_patches', 'stats_stub'].each do |req|
-  require File.dirname(__FILE__) + "/#{req}"
-end
-
-module DocR  
-  # Main class
-  class Parser    
-    attr_accessor :hierarchy, :stats, :tokens, :structured_tokens
-    
-    # Grab the arguments from the DocR init script or
-    # another calling script, feed them to RDoc for
-    # parsing, then generate the documentation.
-    # 
-    # Right now, the guts of this method relies on a lot of 
-    # the current RDoc, but I'm going to slowly replace
-    # each piece of it with our own functionality.
-    def initialize(options)
-      @options = options
-      raise "No files to document!" if @options[:files] == [] || @options[:files] == nil
+module DocR
+  class Parser
+    def self.parse(code)
+      setup_parser
+      setup_structure
       
-      # Setup the analyzed tokens array so we can keep track of which methods we've already
-      # taken a look at...
-      @processed_tokens = []
+      tree = @parser.parse code
+      walk tree
       
-      # Setup a hash for a properly structured, unabmbiguous token structure.
-      # As nice as CodeObjects is, it references some things twice.
-      @tokens = {:class => [], :module => [], :method => []}
-      @structured_tokens = {}
-      
-      r = RDoc::RDoc.new
-      
-      # Instantiate our little patched Stats class...
-      @stats = DocR::Stats.new
-      r.stats = @stats
-      
-      # Setup any options we need here...
-      Options.instance.parse(["--tab-width", 2], {})
-            
-      # We have to use #send because #parse_files is private
-      #
-      # TODO: 1.9 compatibility
-      parsed_structure = r.send(:parse_files, RDocOptionsMock.new(options[:files]))
-      
-      # Analyze it, Spiderman!
-      process parsed_structure
-      
-      # Generate the documentation!
-      generate
+      pp @classes
+      pp @modules
     end
     
-    def parse_files(options)
-      file_list = normalized_file_list(options, files, true)
+    def self.walk(tree, parent_label=nil)
+      tree.values.each do |token|
+        case token[0]
+        when :class
+          @classes[token[1]] = add_class(token) 
+        when :module
+          @modules[token[1]] = add_module(token)
+        end
+      end
+    end
+    
+    def self.add_class(token)
+      {:comments => token.comments}.merge tokens_in(token.scope.block)
+    end
+    
+    def self.add_module(token)
+      {:comments => token.comments}.merge tokens_in(token.scope)
+    end
+    
+    def self.add_method(token)
+      {:comments => value.comments, :args => normalize_args(value.scope.block.args)}
+    end
+    
+    def self.setup_parser
+      @parser ||= RubyParser.new
+    end
+    
+    def self.setup_structure
+      @modules = {}
+      @classes = {}
+    end
+    
+    def self.tokens_in(parent)
+      tokens = {:methods => {:class => {}, :instance => {}}, :classes => {}, :modules => {}}
 
-      file_info = []
-
-      file_list.each do |fn|
-        content = if RUBY_VERSION >= '1.9' then
-                    File.open(fn, "r:ascii-8bit") { |f| f.read }
-                  else
-                    File.read fn
-                  end
-
-        if /coding:\s*(\S+)/ =~ content[/\A(?:.*\n){0,2}/]
-          if enc = Encoding.find($1)
-            content.force_encoding(enc)
+      # Does it even have anything in it?
+      if parent.respond_to?(:values)
+        parent.values.each do |value|
+          next unless value.is_a?(Sexp)
+        
+          if value[0] == :defn || value[0] == :defs
+            # It's a method!
+            if value[1] == s(:self)
+              tokens[:methods][:class][value[2]] = value
+            else
+              tokens[:methods][:instance][value[1]] = add_method(value)
+            end
+          elsif value[0] == :class
+            # d00d it's a class!
+            tokens[:classes][value[1]] = add_class(value)
+          elsif value[0] == :module
+            # Module FTW!!!!
+            tokens[:modules][value[1]] = add_module(value)
+          else
+            # Er, whut?
+            puts "ARGGhHHH!!!!!!!!!!!!!!!!!"
+            pp value
           end
         end
-
-        top_level = TopLevel.new(fn)
-        parser = ParserFactory.parser_for(top_level, fn, content, options, @stats)
-        file_info << parser.scan
-        @stats.num_files += 1
       end
-
-      file_info
+      
+      tokens
     end
     
-    # Method to initialize analysis of the code; passes
-    # structure off to the process_token method which actually
-    # processes each token.
-    def process(hierarchy)
-      @hierarchy = hierarchy
-
-      # Iterate over all the tokens and process them accordingly
-      @hierarchy.each do |hier| 
-        hier.classes.each do |cls| 
-          process_token cls
-        end
-        
-        hier.modules.each do |mod|
-          process_token mod
-        end
-      end
-            
-      # Create a properly nested structure for the tokens to live in
-      @structured_tokens = {}
-      tokens[:class].each do |cls| 
-        @structured_tokens[cls.full_name] = [cls, {
-                        :instance =>  {
-                                        :public => [], 
-                                        :private => [] 
-                                      }, 
-                        :class =>     {
-                                        :public => [], 
-                                        :private => []
-                                      }
-                        }]
-      end
+    def self.normalize_args(args)
       
-      tokens[:module].each do |mod| 
-        @structured_tokens[mod.full_name] = [mod, {
-                        :instance =>  {
-                                        :public => [], 
-                                        :private => []
-                                      }, 
-                        :class =>     {
-                                        :public => [], 
-                                        :private => []
-                                      }
-                        }]
-      end
-      
-      @structured_tokens['[Toplevel]'] = [TopLevel.new, {
-                        :instance =>  {
-                                        :public => [], 
-                                        :private => []
-                                      }, 
-                        :class =>     {
-                                        :public => [], 
-                                        :private => []
-                                      }
-                        }]
-      
-      # Structure method references
-      tokens[:method].each do |method|
-        if (@structured_tokens.has_key?(method.parent.full_name))
-          @structured_tokens[method.parent.full_name][1][method.singleton ? :class : :instance][method.visibility.to_sym] << method
-        else
-          @structured_tokens['[Toplevel]'][1][method.singleton ? :class : :instance][method.visibility.to_sym] << method
-        end
-      end
-      
-      # Show us some code statistics!
-      @stats.print   
-    end
-    
-    # Method to process all the tokens for a token...recursion FTW! :)
-    def process_token(token)
-      # Create a ProcessedToken to make sure we don't process the same thing twice
-      processed_token = ProcessedToken.new(token.name, token.parent)
-      
-      unless @processed_tokens.include?(processed_token)
-        # Add to the right array of tokens
-        tokens[token.classifier] << token
-        @processed_tokens << processed_token
-        
-        # WTF!? :)
-        raise token.method_list[0].singleton.inspect unless token.method_list[0].singleton rescue true
-        
-        # Process the tokens inside of this one accordingly
-        [:method_list, :classes, :modules].each do |meth, type|
-          token.send(meth).each do |item|
-            process_token item
-          end if token.respond_to?(meth)
-        end
-      end
-    end
-    
-    # Generate the output based on the format specified.
-    #
-    # TODO: Have an argument sanity check at startup to make sure we actually have a generator for the format
-    def generate
-      print "Generating documentation..."
-      
-      # Require the generator
-      require File.dirname(__FILE__) + "/generators/#{@options[:output_format]}/generator.rb"
-      
-      # Generate it!
-      generator = DocR::Generator.new(structured_tokens)
-      docs = generator.generate
-      print "done.\n"
-    end
-    
-    def param_names_for(token)
-      # If there are parameters...
-      if token.params
-        # Duplicate
-        params = token.params.dup
-        # Remove parentheses
-        params.gsub!(/\(/, '')
-        params.gsub!(/\)/, '')
-
-        # Multiple parameters?
-        if params.include?(",")
-          # ... split 'em up!
-          params = params.split(",")
-        else
-          # ... or wrap in an array.
-          params = [params]
-        end
-
-        processed_params = []
-
-        params.each do |param|
-          param_value = nil
-
-          # We have a default value...
-          if param.include?('=')
-            param_pieces = param.scan(/(.*)=(.*)/)[0]
-            param = param_pieces[0].strip 
-            param_value = param_pieces[1].strip
-          end
-
-          # Add name and value
-          processed_params << [param, param_value]
-        end
-
-        # No parameters or a fake list?
-        if processed_params == [["", nil]]
-          # Return an empty array...
-          []
-        else
-          # Return the array of parameters
-          processed_params  
-        end
-      else
-        # No params?  Return an empty array
-        []
-      end
     end
   end
-end   
+end
